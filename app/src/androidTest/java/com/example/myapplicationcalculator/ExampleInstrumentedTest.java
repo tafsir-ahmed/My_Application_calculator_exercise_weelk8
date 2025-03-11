417acf1400970fe79488a7f1e149d1ce73b4c0b6
tafsir-ahmed <36663286+tafsir-ahmed@users.noreply.github.com>
2025-03-11 23:38:57 +0200

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/myapplicationcalculator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplicationcalculator/ExampleInstrumentedTest.java
@@ -1,26 +1,67 @@
-package com.example.myapplicationcalculator;
+package com.example.codeseasy.com.simplecalculator;
 
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-import static org.junit.Assert.*;
+    Button buttonAdd, buttonSub, buttonMul, buttonDiv;
+    EditText editTextN1, editTextN2;
+    TextView textView;
+    int num1, num2;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplicationcalculator", appContext.getPackageName());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonAdd = findViewById(R.id.btn_add);
+        buttonSub = findViewById(R.id.btn_sub);
+        buttonMul = findViewById(R.id.btn_mul);
+        buttonDiv = findViewById(R.id.btn_div);
+        editTextN1 = findViewById(R.id.number1);
+        editTextN2 = findViewById(R.id.number2);
+        textView = findViewById(R.id.answer);
+
+        buttonAdd.setOnClickListener(this);
+        buttonSub.setOnClickListener(this);
+        buttonMul.setOnClickListener(this);
+        buttonDiv.setOnClickListener(this);
+
+    }
+
+    public int getIntFromEditText(EditText editText) {
+        if (editText.getText().toString().equals("")) {
+            Toast.makeText(this, "Enter number", Toast.LENGTH_SHORT).show();
+            return 0;
+        } else
+            return Integer.parseInt(editText.getText().toString());
+    }
+
+    @Override
+    public void onClick(View view) {
+        num1 = getIntFromEditText(editTextN1);
+        num2 = getIntFromEditText(editTextN2);
+        switch (view.getId()) {
+            case R.id.btn_add:
+                textView.setText("Answer = " + (num1 + num2));
+                break;
+            case R.id.btn_sub:
+                textView.setText("Answer = " + (num1 - num2));
+                break;
+            case R.id.btn_mul:
+                textView.setText("Answer = " + (num1 * num2));
+                break;
+            case R.id.btn_div:
+                textView.setText("Answer = " + ((float) num1 / (float) num2));
+                break;
+        }
+
     }
 }
